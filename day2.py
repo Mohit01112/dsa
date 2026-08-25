1f4d73f81bc81738e5ba3950c4988a6c221c20b6
Mohit jadhav <rajemohit330@gmail.com>
2026-08-25 23:37:53 +0530

    selection sort

--- day2.py
+++ day2.py
@@ -1,4 +1,4 @@
-## finding a smallest array by normal method
+## finding a smallest element in an array 
 
 def find_smallest(arr):
     smallest=arr[0]
@@ -8,6 +8,15 @@ def find_smallest(arr):
             smallest=arr[i]
             smallest_index=i
     return smallest_index
+## sorting the array
+def selectionsort(arr):
+    new_arr=[]
+    copyarr=list(arr)
+    for i in range (len(arr)):
+        smallest_index=find_smallest(copyarr)
+        new_arr.append(copyarr.pop(smallest_index))
+    return new_arr
 
 arr=[5,3,6,2,10]
 print(find_smallest(arr))
+print(selectionsort(arr))
