2f16924b6d939621bf9f1594966b00600bf946f2
Mohit jadhav <rajemohit330@gmail.com>
2026-09-03 22:46:51 +0530

    trees

---
+++ trees1.py
@@ -0,0 +1,18 @@
+
+from os import listdir
+from os.path import isfile,join
+from collections import deque
+
+def printnames(start_dir):
+    search_queue=deque()
+    search_queue.append(start_dir)
+    while search_queue:
+        dir=search_queue.popleft()
+        for file in listdir(dir):
+            fullpath=join(dir,file)
+            if isfile(fullpath):
+                print(fullpath)
+            else:
+                search_queue.append(fullpath)
+
+printnames(r"C:\Users\rajem\Downloads")
